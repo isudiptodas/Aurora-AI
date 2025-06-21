6fbf10b21c1a526d0e85af2b1438178e9c457bc9
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-06-22 00:14:28 +0530

    added login code in route.ts

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,7 +2,7 @@ import { connectDb } from "@/config/connectDb";
 import { User } from "@/models/User";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
-
+import jwt from 'jsonwebtoken';
 export async function POST(req: NextRequest) {
 
     const body = await req.json();
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
         const { name, email, password } = body;
 
         try {
-            const found = await User.findOne({email});
+            const found = await User.findOne({ email });
 
             if (found) {
                 return NextResponse.json({
@@ -45,7 +45,41 @@ export async function POST(req: NextRequest) {
         }
     }
     else if (type === 'login') {
+        const { email, password } = body;
 
+        const found = await User.findOne({ email });
+
+        if (!found) {
+            return NextResponse.json({
+                success: false,
+                message: "User not found"
+            }, { status: 404 });
+        }
+
+        const matched = await bcrypt.compare(password, found.password);
+
+        if (!matched) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid password"
+            }, { status: 401 });
+        }
+
+        const token = jwt.sign({email: found.email}, process.env.JWT_SECRET as string, {expiresIn: '1d'});
+
+        const res = NextResponse.json({
+            success: true,
+            message: "Login successful"
+        }, {status: 200});
+
+        res.cookies.set('token', token, {
+            sameSite: 'strict',
+            httpOnly: true,
+            secure: true,
+            maxAge: 86400000
+        });
+
+        return res;
     }
     else {
         return NextResponse.json({

66bfef5b402625e3831bb8b04b79331572e4f8d9
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-06-23 00:47:22 +0530

    Update astraConnect.ts

--- src/config/astraConnect.ts
+++ src/config/astraConnect.ts
@@ -2,6 +2,6 @@ import { DataAPIClient } from "@datastax/astra-db-ts";
 
 // Initialize the client
 const client = new DataAPIClient(process.env.ASTRA_DB_TOKEN as string);
-const db = client.db(process.env.ASTRA_DB_ENDPOINT as string, {
+export const db = client.db(process.env.ASTRA_DB_ENDPOINT as string, {
     namespace: process.env.ASTRA_DB_KEYSPACE as string
 });

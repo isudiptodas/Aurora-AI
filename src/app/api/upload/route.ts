7cf7db72b51229985f4298e4accb9f8e83129bc5
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-06-23 23:52:05 +0530

    added code for vector embedding conversion

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -3,6 +3,8 @@ import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube";
 import { db } from "@/config/astraConnect";
 import { connectDb } from "@/config/connectDb";
+import OpenAI from "openai";
+import { Set } from "@/models/Set";
 
 export async function POST(req: NextRequest) {
 
@@ -11,55 +13,222 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const { type } = body;
 
+    const OPENAI_API = process.env.OPENAI_API_KEY as string;
+
+    const openai = new OpenAI({
+        apiKey: OPENAI_API
+    });
+
     if (type === 'pdf') {
-        const { name, pdfText } = body;
+        const { name, pdfText, today, userEmail, original } = body;
 
         const splitter = new RecursiveCharacterTextSplitter({
-            chunkSize: 20,
-            chunkOverlap: 5
+            chunkSize: 500,
+            chunkOverlap: 20
         });
 
-        const output = await splitter.splitText(pdfText);
+        const result = await splitter.splitText(pdfText);
 
-        return NextResponse.json({
-            success: true,
-            message: "Successful"
-        }, { status: 200 });
+        const list = db.listCollections();
+        const found = (await list).some((coll) => coll?.name === name);
+
+        if (found) {
+            return NextResponse.json({
+                success: false,
+                message: "Set with same name exists"
+            }, { status: 401 });
+        }
+
+        const newCol = await db.createCollection(name, {
+            vector: {
+                dimension: 1536,
+                metric: 'dot_product'
+            }
+        });
+
+        try {
+            for (let text of result) {
+                const embedding = await openai.embeddings.create({
+                    model: "text-embedding-3-small",
+                    input: text,
+                    encoding_format: "float",
+                });
+
+                const embed = embedding.data[0].embedding;
+                const resp = await newCol.insertOne({
+                    $vector: embed,
+                    text: text
+                });
+            }
+
+            const newSet = new Set({
+                setName: name,
+                userEmail: userEmail,
+                setType: type,
+                vectorCollectionName: name,
+                dateCreated: today,
+                original: original
+            });
+
+            await newSet.save();
+
+            return NextResponse.json({
+                success: true,
+                message: "Successful"
+            }, { status: 200 });
+        } catch (err) {
+            return NextResponse.json({
+                success: false,
+                message: "Something went wrong"
+            }, { status: 500 });
+        }
     }
     else if (type === 'web') {
+        const { name, web, today, userEmail, original } = body;
+
+        try {
+
+            const list = db.listCollections();
+            const found = (await list).some((coll) => coll?.name === name);
+
+            if (found) {
+                return NextResponse.json({
+                    success: false,
+                    message: "Set with same name exists"
+                }, { status: 401 });
+            }
+
+            const newCol = await db.createCollection(name, {
+                vector: {
+                    dimension: 1536,
+                    metric: 'dot_product'
+                }
+            });
+
+            const splitter = new RecursiveCharacterTextSplitter({
+                chunkSize: 500,
+                chunkOverlap: 20
+            });
+
+            const result = await splitter.splitText(web);
+
+            for (let text of result) {
+                const embedding = await openai.embeddings.create({
+                    model: "text-embedding-3-small",
+                    input: text,
+                    encoding_format: "float",
+                });
+
+                const embed = embedding.data[0].embedding;
+                const resp = await newCol.insertOne({
+                    $vector: embed,
+                    text: text
+                });
+            }
+
+            const newSet = new Set({
+                setName: name,
+                userEmail: userEmail,
+                setType: type,
+                vectorCollectionName: name,
+                dateCreated: today,
+                original: original
+            });
+
+            await newSet.save();
+
+
+            return NextResponse.json({
+                success: true,
+                message: "Successful"
+            }, { status: 200 });
+        } catch (err) {
+            console.log(`Error -> `, err);
+            return NextResponse.json({
+                success: false,
+                message: "Something went wrong"
+            }, { status: 500 });
+        }
 
     }
     else if (type === 'youtube') {
-        const { name, youtube } = body;
+        const { name, youtube, today, userEmail } = body;
 
         try {
-            const loader = YoutubeLoader.createFromUrl('https://youtu.be/6biMWgD6_JY?si=7IKBiyOCSTkezmYX', {
+            const loader = YoutubeLoader.createFromUrl(youtube, {
                 addVideoInfo: false,
                 language: "en",
             });
             const result = await loader.load();
 
-            const splitter = new RecursiveCharacterTextSplitter({
-                chunkSize: 20,
-                chunkOverlap: 5
-            });
+            if (result && result[0].pageContent) {
+                const splitter = new RecursiveCharacterTextSplitter({
+                    chunkSize: 500,
+                    chunkOverlap: 20
+                });
 
-            const output = await splitter.splitText(result[0].pageContent);
-            
-            return NextResponse.json({
-                success: true,
-                message: "Successfull",
-                text: output
-            }, { status: 200 });
+                const output = await splitter.splitText(result[0].pageContent);
+
+                const list = db.listCollections();
+                const found = (await list).some((coll) => coll?.name === name);
+
+                if (found) {
+                    return NextResponse.json({
+                        success: false,
+                        message: "Set with same name exists"
+                    }, { status: 401 });
+                }
+
+                const newCol = await db.createCollection(name, {
+                    vector: {
+                        dimension: 1536,
+                        metric: 'dot_product'
+                    }
+                });
+
+                for (let text of output) {
+                    const embedding = await openai.embeddings.create({
+                        model: "text-embedding-3-small",
+                        input: text,
+                        encoding_format: "float",
+                    });
+
+                    const embed = embedding.data[0].embedding;
+                    const resp = await newCol.insertOne({
+                        $vector: embed,
+                        text: text
+                    });
+                }
+
+                const newSet = new Set({
+                    setName: name,
+                    userEmail: userEmail,
+                    setType: type,
+                    vectorCollectionName: name,
+                    dateCreated: today,
+                    original: youtube
+                });
+
+                await newSet.save();
+
+                return NextResponse.json({
+                    success: true,
+                    message: "Successfull",
+                }, { status: 200 });
+            }
 
         } catch (err) {
-            console.log(`error detected`);
+            //console.log(`error detected`);
+            return NextResponse.json({
+                success: false,
+                message: "Something went wrong",
+            }, { status: 500 });
         }
     }
 
     return NextResponse.json({
         success: false,
-        message: "Invalid request"
-    }, { status: 400 });
+        message: "Invalid request type !"
+    }, { status: 401 });
 
 }
